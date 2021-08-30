acb56df3ed6404c04d6e4d32d928aa9e3d04f21d
beejay321 <jibodu.busola@gmail.com>
2021-08-30 20:55:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Login from "./components/Login";
+// import Login from "./components/Login";
 import LandingPage from "./components/LandingPage";
 import Footer from "./components/Footer";
 import Dashboard from "./components/DashBoard";
 import DetailPage from "./components/detailPage";
 import Register from "./components/Register";
+import UpdateProfile from "./components/UpdateProfile";
+import CreateService from "./components/CreateService";
+import MyOrders from "./components/MyOrders";
+import MyProfile from "./components/MyProfile";
+import AboutUs from "./components/AboutUs";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -16,9 +22,14 @@ function App() {
       <Router>
         <NavBar />
         <Route path="/" exact component={LandingPage} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/login" exact render={(routerProps) => <Login />} />
-        <Route path="/register" exact render={(routerProps) => <Register />} />
+        <Route path="/dashboard" render={(routerProps) => <Dashboard />} />
+        <Route path="/aboutUs" component={AboutUs} />
+        <Route path="/login" render={(routerProps) => <Login />} />
+        <Route path="/register" render={(routerProps) => <Register />} />
+        <Route path="/updateProfile" render={(routerProps) => <UpdateProfile />} />
+        <Route path="/createService" render={(routerProps) => <CreateService />} />
+        <Route path="/MyProfile" render={(routerProps) => <MyProfile />} />
+        <Route path="/myOrders" render={(routerProps) => <MyOrders />} />
         <Route render={(routerProps) => <DetailPage title="DETAILS" {...routerProps} />} path="/details/:projectId" />
         {/* <Login /> */}
         <Footer />
