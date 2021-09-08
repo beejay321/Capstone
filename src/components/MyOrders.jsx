8a3bdb0c4b2b65fc71a35957b28ad02b0b4c2de0
beejay321 <jibodu.busola@gmail.com>
2021-09-08 09:50:37 +0200

    Create My Orders

---
+++ src/components/MyOrders.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Form, Container, Row, Col, Button, Modal, Card } from "react-bootstrap";
+
+const MyOrders = (props) => {
+  return (
+    <>
+      <Container className="py-3" style={{ minHeight: "100vh" }}>
+        <h2>My Orders</h2>
+      </Container>
+    </>
+  );
+};
+export default MyOrders;
