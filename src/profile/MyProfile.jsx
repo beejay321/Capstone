3ca746bf2f0ebff63ed7ec1690f573d7c453e3a7
beejay321 <jibodu.busola@gmail.com>
2021-10-10 23:51:20 +0200

    Update

--- src/profile/MyProfile.jsx
+++ src/profile/MyProfile.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Form, Container, Row, Col, Button, Image, Card, ButtonGroup, ToggleButton } from "react-bootstrap";
 import { Link, withRouter } from "react-router-dom";
 import "./profilepage.css";
+import NavBar from "../components/NavBar";
 import MyProfileCard from "./myProfileCard";
 import ExperienceCard from "./ExperienceCard";
 import EducationCard from "./EducationCard";
@@ -9,14 +10,17 @@ import PublicationCard from "./PublicationCard";
 import SkillsCard from "./SkillsCard";
 import CertificationCard from "./Certification";
 import MyProjects from "./MyProjects";
+import MyBids from "./MyBids";
 import ChatLists from "../components/chatLists";
 import ChatBox from "../components/chatBox";
+import Footer from "../components/Footer";
 
 const ADDRESS = "http://localhost:3255";
 
 const MyProfile = (props) => {
   const [user, setUser] = useState("");
   const [education, setEducation] = useState("");
+  const [experience, setExperience] = useState("");
   const [projects, setProject] = useState([]);
   const [skills, setSkills] = useState([]);
   const [language, setLanguage] = useState([]);
@@ -27,7 +31,7 @@ const MyProfile = (props) => {
   const [selectedRoom, setSelectedRoom] = useState(null);
   const [roomHistory, setRoomHistory] = useState(null);
   const [chats, setChats] = useState(null);
-  const [dataSource, setDataSource] = useState(null);
+  const [myBids, setMyBids] = useState(null);
 
   // ${match.params.projectId}
 
@@ -46,8 +50,11 @@ const MyProfile = (props) => {
           setUser(data);
           console.log(data.projects);
           setProject(data.projects);
+          setMyBids(data.myBids);
           setSkills(data.skills);
           setLanguage(data.languages);
+          setEducation(data.education);
+          setExperience(data.experience);
         }
       } catch (error) {
         console.log(error);
@@ -144,132 +151,141 @@ const MyProfile = (props) => {
 
   return (
     <>
-      <Container className="py-3" style={{ minHeight: "100vh" }}>
-        {/* <h2>My Profile </h2> */}
-        <Row className="my-0">
-          <Col xs={3}>
-            <Row>
-              <div className="d-flex ">
-                <Button variant="outline-secondary" type="submit" onClick={() => setClient(false)}>
-                  As Freelancer
-                </Button>
-                <Button variant="outline-secondary" type="submit" onClick={() => setClient(true)}>
-                  As Client
-                </Button>
-              </div>
-            </Row>
-          </Col>
-        </Row>
-
-        <Row className="">
-          <Col xs={3} className="    ">
-            <Row className=" my-3 py-4 profileColumn">
-              <div className="  pb-2 d-flex justify-content-center">
-                <div className="  profileImageDiv ">
-                  <Image className="profileImage" src={user.picture} fluid />
+      <NavBar />
+      <div className="py-3 profilePage">
+        <Container className="py-3 profilePage" style={{ minHeight: "100vh" }}>
+          {/* <h2>My Profile </h2> */}
+          <Row className="my-0">
+            <Col xs={3}>
+              <Row>
+                <div className="d-flex ">
+                  <Button variant="outline-secondary" type="submit" onClick={() => setClient(false)}>
+                    As Freelancer
+                  </Button>
+                  <Button variant="outline-secondary" type="submit" onClick={() => setClient(true)}>
+                    As Client
+                  </Button>
                 </div>
-              </div>
-              <div className=" d-flex justify-content-center">
-                <h5>{user.firstname}</h5>
-                <h5 className="px-2">{user.lastname}</h5>
-              </div>
-              <div className=" d-flex justify-content-center ">
-                <h6>{user.headline}</h6>
-              </div>
-              <div className="py-1">
-                <p className="">{user.location}</p>
-              </div>
+              </Row>
+            </Col>
+          </Row>
 
-              {language &&
-                language.map((l) => (
-                  <div className=" py-1   ">
-                    <span className="   ">
-                      <strong>Languages</strong>
-                    </span>
-                    <p className=" py-1  summaryBox ">{user.languages}</p>
+          <Row className="d-flex gap-1">
+            <Col xs={3} className="    ">
+              <Row className=" my-3 py-4 profileColumn">
+                <div className="  pb-2 d-flex justify-content-center">
+                  <div className="  profileImageDiv ">
+                    <Image className="profileImage" src={user.picture} fluid />
                   </div>
-                ))}
-              <hr className=" my-1 " />
-              <div className=" py-1   ">
-                <div className="  d-flex justify-content-between ">
-                  <span className="   ">
-                    {" "}
-                    <strong>Open Projects</strong>{" "}
-                  </span>
-                  <p className="  px-1  ">2</p>
                 </div>
-                <div className="  d-flex justify-content-between">
-                  <span className="   ">
-                    {" "}
-                    <strong>Finished Projects</strong>
-                  </span>
-                  <p className="  px-1  ">2</p>
+                <div className=" d-flex justify-content-center">
+                  <h5>{user.firstname}</h5>
+                  <h5 className="px-2">{user.lastname}</h5>
                 </div>
-                <div className="  d-flex justify-content-between ">
-                  <span className="   ">
-                    {" "}
-                    <strong>All Projects</strong>
-                  </span>
-                  <p className="  px-1  ">2</p>
+                <div className=" d-flex justify-content-center ">
+                  <h6>{user.headline}</h6>
                 </div>
-              </div>
-              <div className="d-flex justify-content-end">
-                {localStorage.getItem("id") === user._id ? (
-                  <Button variant="primary" type="submit" onClick={() => props.history.push("/updateProfile/613109f216858d24880aaa2a")}>
-                    Edit Profile
-                  </Button>
-                ) : (
-                  <Button variant="primary" type="submit" onClick={showChatBox}>
-                    Message
-                  </Button>
-                )}
-              </div>
-            </Row>
+                <div className="py-1">
+                  <p className="">{user.location}</p>
+                </div>
 
-            <Row className="  profileColumn">
-              <MyProfileCard title="Skills" content={<SkillsCard title="Skills" skills={skills} user={user} />} />
-            </Row>
-            <Row className=" my-3 profileColumn">
-              <MyProfileCard title="Cert" content={<CertificationCard title="Cert" user={user} />} />
-            </Row>
-          </Col>
-          <Col xs={7} className="mx-5">
-            {client && <MyProfileCard title="Projects" user={user} content={<MyProjects title="Projects" projects={projects} user={user} />} />}
-            <div className="my-3 py-2 px-1 summaryBox " style={{ minHeight: "15rem" }}>
-              <div className="mx-2 ">
-                <h4>Reviews</h4>
-              </div>{" "}
-              <hr className=" my-2 " />
-              <div className="  mt-5 d-flex justify-content-center ">
-                <p>No Reviews Yet</p>
+                {language &&
+                  language.map((l) => (
+                    <div className=" py-1   ">
+                      <span className="   ">
+                        <strong>Languages</strong>
+                      </span>
+                      <p className=" py-1  summaryBox ">{user.languages}</p>
+                    </div>
+                  ))}
+                <hr className=" my-1 " />
+                <div className=" py-1   ">
+                  <div className="  d-flex justify-content-between ">
+                    <span className="   ">
+                      {" "}
+                      <strong>Open Projects</strong>{" "}
+                    </span>
+                    <p className="  px-1  ">2</p>
+                  </div>
+                  <div className="  d-flex justify-content-between">
+                    <span className="   ">
+                      {" "}
+                      <strong>Finished Projects</strong>
+                    </span>
+                    <p className="px-1">2</p>
+                  </div>
+                  <div className="d-flex justify-content-between ">
+                    <span className="   ">
+                      {" "}
+                      <strong>All Projects</strong>
+                    </span>
+                    <p className="px-1">2</p>
+                  </div>
+                </div>
+                <div className="d-flex justify-content-end">
+                  {localStorage.getItem("id") === user._id ? (
+                    <Button className="editButton" variant="primary" type="submit" onClick={() => props.history.push("/updateProfile/613109f216858d24880aaa2a")}>
+                      Edit Profile
+                    </Button>
+                  ) : (
+                    <Button className="editButton" variant="primary" type="submit" onClick={showChatBox}>
+                      Message
+                    </Button>
+                  )}
+                </div>
+              </Row>
+
+              <Row className="  profileColumn">
+                <MyProfileCard title="Skills" content={<SkillsCard title="Skills" skills={skills} user={user} />} />
+              </Row>
+              <Row className=" my-3 profileColumn">
+                <MyProfileCard title="Cert" content={<CertificationCard title="Cert" user={user} />} />
+              </Row>
+            </Col>
+            <Col xs={7} className="">
+              {client ? (
+                <MyProfileCard title="Projects" user={user} content={<MyProjects title="Projects" projects={projects} user={user} />} />
+              ) : (
+                <MyProfileCard title="Projects" user={user} content={<MyBids title="Bids" myBids={myBids} user={user} />} />
+              )}
+
+              <MyProfileCard title="Experience" user={user} content={<ExperienceCard title="Experience" user={user} experience={experience} />} />
+              <MyProfileCard title="Education" user={user} content={<EducationCard title="Education" user={user} education={education} />} />
+              <MyProfileCard title="Publication" user={user} content={<PublicationCard title="Publication" user={user} />} />
+              <div className="my-3 py-2 px-1 profileColumn " style={{ minHeight: "15rem" }}>
+                <div className="mx-2 ">
+                  <h4>Reviews</h4>
+                </div>{" "}
+                <hr className=" my-2 " />
+                <div className="    mt-5 d-flex justify-content-center ">
+                  <p>No Reviews Yet</p>
+                </div>
               </div>
-            </div>
-            <MyProfileCard title="Experience" user={user} content={<ExperienceCard title="Experience" user={user} />} />
-            <MyProfileCard title="Education" user={user} content={<EducationCard title="Education" user={user} />} />
-            <MyProfileCard title="Publication" user={user} content={<PublicationCard title="Publication" user={user} />} />
-          </Col>
-          <Col xs={3} className="">
-            <ChatBox selectedRoom={selectedRoom} roomHistory={roomHistory} show={showChat} setShowChat={setShowChat} user={user} firstname={user.firstname} lastname={user.lastname} />
-          </Col>
-          {localStorage.getItem("id") === user._id ? (
-            <ChatLists
-              setShowChatList={setShowChatList}
-              chats={chats}
-              getRooms={getRooms}
-              continueRoom={continueRoom}
-              selectedRoom={selectedRoom}
-              roomHistory={roomHistory}
-              show={showChat}
-              setShowChat={setShowChat}
-              user={user}
-              firstname={user.firstname}
-              lastname={user.lastname}
-            />
-          ) : (
-            ""
-          )}
-        </Row>
-      </Container>
+            </Col>
+            <Col xs={3} className="">
+              <ChatBox selectedRoom={selectedRoom} roomHistory={roomHistory} showChat={showChat} setShowChat={setShowChat} user={user} firstname={user.firstname} lastname={user.lastname} />
+            </Col>
+            {localStorage.getItem("id") === user._id ? (
+              <ChatLists
+                setShowChatList={setShowChatList}
+                chats={chats}
+                getRooms={getRooms}
+                continueRoom={continueRoom}
+                selectedRoom={selectedRoom}
+                roomHistory={roomHistory}
+                showChat={showChat}
+                setShowChat={setShowChat}
+                user={user}
+                firstname={user.firstname}
+                lastname={user.lastname}
+              />
+            ) : (
+              ""
+            )}
+          </Row>
+        </Container>
+      </div>
+      <Footer />
     </>
   );
 };
