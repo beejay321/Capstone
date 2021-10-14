b71805f0e60ab8ef7377c843730562d10789b76c
beejay321 <jibodu.busola@gmail.com>
2021-10-15 00:02:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router } from "react-router-dom";
-
-import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import AboutUs from "./components/AboutUs";
+import Dashboard from "./components/DashBoard";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Router>
-        <NavBar />
+        {/* <NavBar /> */}
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/aboutUs" component={AboutUs} />
+        <Route path="/dashboard" render={(routerProps) => <Dashboard {...routerProps} />} />
 
         <Footer />
       </Router>
