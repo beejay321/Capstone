14af4f05cf402cf5bbccc21a8ef82fbaedafe41a
beejay321 <jibodu.busola@gmail.com>
2021-10-16 23:45:53 +0200

    Update

--- src/components/checkout.jsx
+++ src/components/checkout.jsx
@@ -10,12 +10,13 @@ const ADDRESS = "http://localhost:3255";
 const Checkout = ({ match, history }) => {
   const [bidder, setBidder] = useState("");
   const [checkout, setCheckOut] = useState(false);
-  const [price, setPrice] = useState("50");
+  const [price, setPrice] = useState("");
   const [projectDetails, setProjectDetails] = useState("");
   const [project, setProject] = useState("");
   const [hidePaypal, setHidePaypal] = useState(false);
   const [showPaymentDiv, setShowPaymentDiv] = useState(false);
   const [paymentDetails, setPaymentDetails] = useState("");
+  const [alert, setAlert] = useState("confirm");
 
   const getProject = async () => {
     try {
@@ -72,7 +73,8 @@ const Checkout = ({ match, history }) => {
       details: projectDetails,
     };
     setPaymentDetails(service);
-    alert("project details saved, you can make payment now");
+    setAlert("saved");
+    // setCheckOut(true);
     setShowPaymentDiv(true);
   };
 
@@ -116,13 +118,30 @@ const Checkout = ({ match, history }) => {
         </div>
       ) : (
         <Container className=" CheckoutContainer  mb-5">
-          <Alert className="my-3" variant="success">
-            <Alert.Heading>Confirm details of the project with the Freelancer!!</Alert.Heading>
-            <p>
-              Be sure to have a chat with the freelancer about the details of the project and confirm their availability and ability to carry out the task. Payment made will be held by the platform
-              until project is completed .
-            </p>
-          </Alert>
+          {alert === "confirm" && (
+            <Alert className="my-3" variant="success">
+              <Alert.Heading>Confirm details of the project with the Freelancer!!</Alert.Heading>
+              <p>
+                Be sure to have a chat with the freelancer about the details of the project and confirm their availability and ability to carry out the task. Payment made will be held by the platform
+                until project is completed .
+              </p>
+            </Alert>
+          )}
+          {alert === "saved" && (
+            <Alert className="my-3" variant="success">
+              <Alert.Heading>Project details saved, you can make payment now</Alert.Heading>
+            </Alert>
+          )}
+          {alert === "paymentMade" && (
+            <Alert className="my-3" variant="success">
+              <Alert.Heading>Payment made succesfully</Alert.Heading>
+            </Alert>
+          )}
+          {alert === "emailSent" && (
+            <Alert className="my-3" variant="success">
+              <Alert.Heading>Confirmation email has been sent to freelancer</Alert.Heading>
+            </Alert>
+          )}
 
           <Row className="d-flex gap-5 px-4 my-5">
             <Col xs={6} className=" CheckoutBox p-3">
@@ -170,25 +189,22 @@ const Checkout = ({ match, history }) => {
                   </Form>
                 </div>
                 <div className="d-flex justify-content-end gap-1">
+                  {!checkout &&
                   <Button variant="success" type="submit" onClick={makePayment}>
-                    Submit{" "}
-                  </Button>
+                    Proceed to payment
+                  </Button>}
                 </div>
               </div>
             </Col>
             {showPaymentDiv && (
               <Col className=" CheckoutBox p-3">
-                <h3>Payment Method</h3>
-                {/* <div className=""> */}
-                <Form>
-                  <div className="mb-3">
-                    <Form.Check type="radio" id="default-radio" label="Cash On Delivery" />
-                  </div>
-                </Form>
+                <h3 className="pb-2">Payment Method</h3>
+
                 <div>
-                  {checkout ? (
-                    <PayPal paymentDetails={paymentDetails} setHidePaypal={setHidePaypal} project={project} bidder={bidder} price={price} projectDetails={projectDetails} />
-                  ) : (
+                  {checkout && (
+                    <PayPal setAlert={setAlert} paymentDetails={paymentDetails} setHidePaypal={setHidePaypal} project={project} bidder={bidder} price={price} projectDetails={projectDetails} />
+                  )}
+                  {/* ) : (
                     <Button
                       onClick={() => {
                         setCheckOut(true);
@@ -196,7 +212,7 @@ const Checkout = ({ match, history }) => {
                     >
                       Make Payment
                     </Button>
-                  )}
+                  )} */}
                 </div>
                 {/* </div> */}
               </Col>
