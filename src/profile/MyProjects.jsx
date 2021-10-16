ac88e313619e8fffc68418b2410a83f826b493d3
beejay321 <jibodu.busola@gmail.com>
2021-10-16 23:47:00 +0200

    Update MyProjects.jsx

--- src/profile/MyProjects.jsx
+++ src/profile/MyProjects.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Table } from "react-bootstrap";
 // import "./dashboard.css";
 import { Link } from "react-router-dom";
 import EditModal from "./EditModal";
@@ -14,18 +15,45 @@ const MyProjects = (props) => {
       <div className=" my-2 py-2 px-1 profileColumn " style={{ minHeight: "15rem" }}>
         <div className=" mx-2 d-flex justify-content-between ">
           <h4>{props.title}</h4>
-          <EditModal title={props.title} />
+          {/* <EditModal title={props.title} /> */}
         </div>
         <hr className=" my-2 " />
         {props.projects ? (
           <div>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Summary</th>
+                  <th>Location</th>
+                  {/* <th>Status</th> */}
+                </tr>
+              </thead>
+              {props.projects.map((p) => (
+                <tbody>
+                  <tr>
+                    <td>{p.title}</td>
+                    <td>{p.location}</td>
+                    <td>{p.category}</td>
+                  </tr>
+                </tbody>
+              ))}
+            </Table>
+          </div>
+        ) : (
+          <div className="  mt-5 d-flex justify-content-center ">
+            <p>No Projects Yet</p>
+          </div>
+        )}
+        {/* {props.projects ? (
+          <div>
             {props.projects.map((p) => (
               <div className=" mx-2 d-flex justify-content-between ">
-                  <div className="py-1  my-1  ">
-                <Link className="projectLink" to={`/details/${p._id}`}>
+                <div className="py-1  my-1  ">
+                  <Link className="projectLink" to={`/details/${p._id}`}>
                     <span>{p.title}</span>
-                </Link>
-                  </div>
+                  </Link>
+                </div>
                 <div className="py-1  my-1  ">
                   <span>{p.summary} </span>
                 </div>
@@ -39,7 +67,7 @@ const MyProjects = (props) => {
           <div className="  mt-5 d-flex justify-content-center ">
             <p>No Projects Yet</p>
           </div>
-        )}
+        )} */}
       </div>
     </>
   );
