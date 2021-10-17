fe600617eaa8658126e73378e26ace66464e2ef9
beejay321 <jibodu.busola@gmail.com>
2021-10-17 10:09:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import PostProject from "./components/postProjects";
 import DetailPage from "./components/detailPage";
 import Checkout from "./components/checkout";
 import ConfirmDetails from "./components/confirmDetails";
+import Login from "./components/Login";
+// import MyProfile from "./profile/MyProfile.jsx";
+// import UpdateProfile from "./components/UpdateProfile";
+// import ChatBox from "./components/chatBox";
 
 
 function App() {
@@ -20,10 +24,13 @@ function App() {
         <Route path="/aboutUs" component={AboutUs} />
         <Route path="/dashboard" render={(routerProps) => <Dashboard {...routerProps} />} />
         <Route path="/postproject" render={(routerProps) => <PostProject {...routerProps} />} />
+        <Route path="/login" render={(routerProps) => <Login {...routerProps} />} />
         <Route path="/details/:projectId" render={(routerProps) => <DetailPage title="DETAILS" {...routerProps} />} />
         <Route path="/checkout/:projectId/:bidId" render={(routerProps) => <Checkout {...routerProps} />} />
         <Route path="/confirmProjectDetails/:bidderId" render={(routerProps) => <ConfirmDetails {...routerProps} />} />
-
+        {/* <Route path="/users/:id" render={(routerProps) => <MyProfile {...routerProps} />} /> */}
+        {/* <Route path="/updateProfile" render={(routerProps) => <UpdateProfile {...routerProps} />} /> */}
+        {/* <Route path="/chat" render={(routerProps) => <ChatBox {...routerProps} />} /> */}
         {/* <Footer /> */}
       </Router>
     </>
