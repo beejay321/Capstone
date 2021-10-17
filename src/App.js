ee197acfb72aec20dd16982b71de9799848c8553
beejay321 <jibodu.busola@gmail.com>
2021-10-17 10:39:42 +0200

    Delete Checkout

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import AboutUs from "./components/AboutUs";
 import Dashboard from "./components/DashBoard";
 import PostProject from "./components/postProjects";
 import DetailPage from "./components/detailPage";
-import Checkout from "./components/checkout";
-import ConfirmDetails from "./components/confirmDetails";
+// import Checkout from "./components/checkout";
+// import ConfirmDetails from "./components/confirmDetails";
 import Login from "./components/Login";
 // import MyProfile from "./profile/MyProfile.jsx";
 // import UpdateProfile from "./components/UpdateProfile";
@@ -26,8 +26,8 @@ function App() {
         <Route path="/postproject" render={(routerProps) => <PostProject {...routerProps} />} />
         <Route path="/login" render={(routerProps) => <Login {...routerProps} />} />
         <Route path="/details/:projectId" render={(routerProps) => <DetailPage title="DETAILS" {...routerProps} />} />
-        <Route path="/checkout/:projectId/:bidId" render={(routerProps) => <Checkout {...routerProps} />} />
-        <Route path="/confirmProjectDetails/:bidderId" render={(routerProps) => <ConfirmDetails {...routerProps} />} />
+        {/* <Route path="/checkout/:projectId/:bidId" render={(routerProps) => <Checkout {...routerProps} />} /> */}
+        {/* <Route path="/confirmProjectDetails/:bidderId" render={(routerProps) => <ConfirmDetails {...routerProps} />} /> */}
         {/* <Route path="/users/:id" render={(routerProps) => <MyProfile {...routerProps} />} /> */}
         {/* <Route path="/updateProfile" render={(routerProps) => <UpdateProfile {...routerProps} />} /> */}
         {/* <Route path="/chat" render={(routerProps) => <ChatBox {...routerProps} />} /> */}
