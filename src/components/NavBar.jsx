8d1e887cfb66d3eb00b5848fc085aaeebf06b0b5
beejay321 <jibodu.busola@gmail.com>
2021-10-18 16:31:16 +0200

    Update navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -39,8 +39,8 @@ const NavBar = (props) => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/aboutUs"> About Us</Nav.Link>
               <Nav.Link href="/dashboard">Find Projects</Nav.Link>
+              <Nav.Link href="/aboutUs"> About Us</Nav.Link>
               <Nav.Link href="/faq">FAQ</Nav.Link>
             </Nav>
 
