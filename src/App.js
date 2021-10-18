5e2b634c949682d4c4638b4a4fc8fec526c68a85
beejay321 <jibodu.busola@gmail.com>
2021-10-18 07:39:32 +0200

    Create  checkout

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Faq from "./components/Faq";
 import Dashboard from "./components/DashBoard";
 import PostProject from "./components/postProjects";
 import DetailPage from "./components/detailPage";
-// import Checkout from "./components/checkout";
+import Checkout from "./components/checkout";
 // import ConfirmDetails from "./components/confirmDetails";
 import Login from "./components/Login";
 import MyProfile from "./profile/MyProfile.jsx";
@@ -27,7 +27,7 @@ function App() {
         <Route path="/postproject" render={(routerProps) => <PostProject {...routerProps} />} />
         <Route path="/login" render={(routerProps) => <Login {...routerProps} />} />
         <Route path="/details/:projectId" render={(routerProps) => <DetailPage title="DETAILS" {...routerProps} />} />
-        {/* <Route path="/checkout/:projectId/:bidId" render={(routerProps) => <Checkout {...routerProps} />} /> */}
+        <Route path="/checkout/:projectId/:bidId" render={(routerProps) => <Checkout {...routerProps} />} />
         {/* <Route path="/confirmProjectDetails/:bidderId" render={(routerProps) => <ConfirmDetails {...routerProps} />} /> */}
         <Route path="/users/:id" render={(routerProps) => <MyProfile {...routerProps} />} />
         {/* <Route path="/updateProfile" render={(routerProps) => <UpdateProfile {...routerProps} />} /> */}
