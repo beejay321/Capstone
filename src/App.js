c908fda228de6ea64b6bb969cab80d03d9b10d67
beejay321 <jibodu.busola@gmail.com>
2022-05-27 15:45:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import LandingPage from "./components/LandingPage";
+import LandingPage from "./components/HomePage/LandingPage";
 import AboutUs from "./components/AboutUs";
 import Faq from "./components/Faq";
-import Dashboard from "./components/DashBoard";
+import Dashboard from "./components/BrowseProjects/DashBoard";
 import PostProject from "./components/postProjects";
-import DetailPage from "./components/detailPage";
-import Checkout from "./components/checkout";
+import DetailPage from "./components/ShowDetail/DetailPage";
+import Checkout from "./components/Checkout/Checkout";
 import ConfirmDetails from "./components/confirmDetails";
 import Login from "./components/Login";
 import MyProfile from "./profile/MyProfile.jsx";
+import EditProject from "./components/EditProject";
 // import UpdateProfile from "./components/UpdateProfile";
 // import ChatBox from "./components/chatBox";
 
@@ -25,6 +26,7 @@ function App() {
         <Route path="/faq" component={Faq} />
         <Route path="/dashboard" render={(routerProps) => <Dashboard {...routerProps} />} />
         <Route path="/postproject" render={(routerProps) => <PostProject {...routerProps} />} />
+        {/* <Route path="/editproject/:projectId" render={(routerProps) => <EditProject {...routerProps} />} /> */}
         <Route path="/login" render={(routerProps) => <Login {...routerProps} />} />
         <Route path="/details/:projectId" render={(routerProps) => <DetailPage title="DETAILS" {...routerProps} />} />
         <Route path="/checkout/:projectId/:bidId" render={(routerProps) => <Checkout {...routerProps} />} />
