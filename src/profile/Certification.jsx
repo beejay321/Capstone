98858ef89f4bd34f12b564f4b8c245391d47ecab
beejay321 <jibodu.busola@gmail.com>
2022-07-15 12:54:28 +0200

    Update Certification.jsx

--- src/profile/Certification.jsx
+++ src/profile/Certification.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import "./profilepage.css";
-import EditModal from "./EditExperience";
+import Edit from "./Edit";
 
 const CertificationCard = (props) => {
-  
-
   return (
     <>
       <div className=" my-2 py-2 px-1 profileColumn " style={{ minHeight: "15rem" }}>
         <div className=" mx-2 d-flex justify-content-between ">
           <h4>{props.title}</h4>
           <div className=" mx-1 d-flex gap-2 ">
-            {localStorage.getItem("id") === props.user._id ? <EditModal title="Add" /> : ""}
-            {localStorage.getItem("id") === props.user._id ? <EditModal title="Edit" /> : ""}
-          </div>        </div>
+            {localStorage.getItem("id") === props.user._id ? <Edit title="Add" /> : ""}
+            {localStorage.getItem("id") === props.user._id ? <Edit title="Edit" /> : ""}
+          </div>{" "}
+        </div>
         <hr className=" my-2 " />
         <div className="  mt-5 d-flex justify-content-center ">
           <p>No Certifications Yet</p>
