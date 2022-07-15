76740954deb016cdff2a8e4ca825520f6cd17d3d
beejay321 <jibodu.busola@gmail.com>
2022-07-15 12:51:51 +0200

    Update PublicationCard.jsx

--- src/profile/PublicationCard.jsx
+++ src/profile/PublicationCard.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./profilepage.css";
-import EditModal from "./EditModal";
+import Edit from "./Edit";
 
 const PublicationCard = (props) => {
-  
-
   return (
     <>
       <div className=" my-2 py-2 px-1 profileColumn " style={{ minHeight: "15rem" }}>
         <div className=" mx-2 d-flex justify-content-between ">
           <h4>{props.title}</h4>
-          {localStorage.getItem("id") === props.user._id ? <EditModal title={props.title} /> : ""}
+          <div className=" mx-1 d-flex gap-2 ">
+            {localStorage.getItem("id") === props.user._id ? <Edit title="Add" /> : ""}
+            {localStorage.getItem("id") === props.user._id ? <Edit title="Edit" /> : ""}
+          </div>{" "}
         </div>
         <hr className=" my-2 " />
         <div>
