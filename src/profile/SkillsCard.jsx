9177b2284d64e87ad4328c5e6fa146d3501fd7a6
beejay321 <jibodu.busola@gmail.com>
2022-07-15 12:52:45 +0200

    Update SkillsCard.jsx

--- src/profile/SkillsCard.jsx
+++ src/profile/SkillsCard.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import "./profilepage.css";
-import EditModal from "./EditModal";
+import { Row, Col, } from "react-bootstrap";
+// import styles from "./profile.module.css";
+import Edit from "./Edit";
 
 const SkillsCard = (props) => {
   return (
     <>
-      <div className="  px-1 summaryBox " style={{ minHeight: "15rem" }}>
+      <div className=" my-2 py-2 px-1 profileColumn " style={{ minHeight: "15rem" }}>
         <div className=" mx-2 d-flex justify-content-between ">
           <h4>{props.title}</h4>
-          {localStorage.getItem("id") === props.user._id ? <EditModal title={props.title} /> : ""}
+          <div className=" mx-1 d-flex gap-3 ">
+            {localStorage.getItem("id") === props.user._id ? <Edit title="Add" /> : ""}
+            {localStorage.getItem("id") === props.user._id ? <Edit title="Edit" /> : ""}
+          </div>{" "}
         </div>
         <hr className=" my-2 " />
-        {props.skills ? (
+        {/* {props.skills ? (
           <div>
             {props.skills &&
               props.skills.map((skill) => (
@@ -24,9 +29,27 @@ const SkillsCard = (props) => {
           </div>
         ) : (
           <div className="  mt-5 d-flex justify-content-center ">
-            <p>No Skills Yet</p>
+            <p>Skills added will help clients choose you for their projects </p>
           </div>
-        )}
+        )} */}
+        {/* <div className=" d-flex justify-content-center"> */}
+        <Row className=" mx-2  skills">
+          <Col xs={2}>JavaScript</Col>
+          <Col xs={2}>CSS</Col>
+          <Col xs={2}>React</Col>
+          <Col xs={2}>Ruby</Col>
+          <Col xs={2}>Photoshop</Col>
+          <Col xs={2}>Indesign</Col>
+          <Col xs={2}>Node</Col>
+          <Col xs={2}>JavaScript</Col>
+          <Col xs={2}>CSS</Col>
+          <Col xs={2}>React</Col>
+          <Col xs={2}>Ruby</Col>
+          <Col xs={2}>Photoshop</Col>
+          <Col xs={2}>Indesign</Col>
+          <Col xs={2}>Node</Col>
+        </Row>
+        {/* </div> */}
       </div>
     </>
   );
