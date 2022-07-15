ea3f044f0a06c2de2ca5a918a01a21e7e9b3fb76
beejay321 <jibodu.busola@gmail.com>
2022-07-15 12:14:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ import DetailPage from "./components/ShowDetail/DetailPage";
 import Checkout from "./components/Checkout/Checkout";
 import ConfirmDetails from "./components/confirmDetails";
 import RegisterPage from "./components/Register/RegisterPage";
+import MyProfile from "./profile/MyProfile.jsx";
+import UpdateProfile from "./profile/UpdateProfile";
 // import Messages from "./components/Message/Messages";
 // import Login from "./components/Register/Login";
-// import MyProfile from "./profile/MyProfile.jsx";
 // import EditProject from "./components/ShowDetail/EditProject.jsx";
-// import UpdateProfile from "./profile/UpdateProfile";
 
 // import ChatBox from "./components/chatBox";
 
@@ -29,13 +29,13 @@ function App() {
         <Route path="/faq" component={Faq} />
         <Route path="/dashboard" render={(routerProps) => <Dashboard {...routerProps} />} />
         <Route path="/postproject" render={(routerProps) => <PostProject {...routerProps} />} />
-        {/* <Route path="/editproject/:projectId" render={(routerProps) => <EditProject {...routerProps} />} /> */}
         <Route path="/register" render={(routerProps) => <RegisterPage {...routerProps} />} />
         <Route path="/details/:projectId" render={(routerProps) => <DetailPage title="DETAILS" {...routerProps} />} />
         <Route path="/checkout/:projectId/:bidId" render={(routerProps) => <Checkout {...routerProps} />} />
         <Route path="/confirmProjectDetails/:bidderId" render={(routerProps) => <ConfirmDetails {...routerProps} />} />
-        {/* <Route path="/users/:id" render={(routerProps) => <MyProfile {...routerProps} />} /> */}
-        {/* <Route path="/updateProfile/:id" render={(routerProps) => <UpdateProfile {...routerProps} />} /> */}
+        <Route path="/users/:id" render={(routerProps) => <MyProfile {...routerProps} />} />
+        <Route path="/updateProfile/:id" render={(routerProps) => <UpdateProfile {...routerProps} />} />
+        {/* <Route path="/editproject/:projectId" render={(routerProps) => <EditProject {...routerProps} />} /> */}
         {/* <Route path="/me/messages" render={(routerProps) => <Messages {...routerProps} />} /> */}
         {/* <Route path="/chat" render={(routerProps) => <ChatBox {...routerProps} />} /> */}
         {/* <Footer /> */}
