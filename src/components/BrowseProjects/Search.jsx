38733acd3ce4055d3a3d18f86e6bd929ef3227b3
beejay321 <jibodu.busola@gmail.com>
2022-07-21 04:22:17 +0200

    Update Search.jsx

--- src/components/BrowseProjects/Search.jsx
+++ src/components/BrowseProjects/Search.jsx
@@ -1,25 +1,26 @@
 import React from "react";
-import { Container, Row, Button, Col, InputGroup, FormControl} from "react-bootstrap";
+import { Container, Row, Button, Col, InputGroup, FormControl } from "react-bootstrap";
+import styles from "./dashboard.module.css";
 
 function Search(props) {
-//   const [query, setQuery] = useState("");
+  //   const [query, setQuery] = useState("");
   // const [category, setCategory] = useState("");
-//   const [projects, setProjects] = useState([]);
+  //   const [projects, setProjects] = useState([]);
 
   return (
-    <div id="searchDiv">
-      <Container className="mt-3 mb-0">
-        <Row>
-          <Col className="d-flex justify-content-between mb-0 gap-2 ">
-            <InputGroup className="searchInput">
-              <FormControl value={props.query} onChange={(e) => props.setQuery(e.target.value)} placeholder="What are your searching for ?" aria-label="Search" aria-describedby="basic-addon2" />
-            </InputGroup>
+    <div id={styles.searchDiv}>
+      <Container className="">
+        {/* <Row> */}
+        <Col className={styles.searchContainer}>
+          <InputGroup className={styles.searchInput}>
+            <FormControl value={props.query} onChange={(e) => props.setQuery(e.target.value)} placeholder="What are your searching for ?" aria-label="Search" aria-describedby="basic-addon2" />
+          </InputGroup>
 
-            <Button id="button-addon2" variant="outline" className="searchButton " onClick={props.searchProjects}>
-              Search
-            </Button>
-          </Col>
-        </Row>
+          <Button id="button-addon2" variant="outline" className={styles.searchButton} onClick={props.searchProjects}>
+            Search
+          </Button>
+        </Col>
+        {/* </Row> */}
       </Container>
     </div>
   );
