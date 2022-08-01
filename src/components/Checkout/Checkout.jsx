a81ea4a89e6e97aede8b7c6668dd1a3341ce5d28
beejay321 <jibodu.busola@gmail.com>
2022-08-01 22:55:50 +0200

    Update Checkout.jsx

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,20 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Alert } from "react-bootstrap";
-// import { Link } from "react-router-dom";
 import "../../styles/checkout.css";
-// import PayPal from "../PayPal";
+import styles from "./checkout.module.css";
 import NavBar from "../NavBar";
 import Footer from "../Footer";
 import PaymentMade from "./PaymentMade";
 import ProjectCheckout from "./ProjectCheckout";
 import ProjectSummary from "./ProjectSummary";
 
-// const ADDRESS = "http://localhost:3255";
+// const MY_APP_API_URL = "http://localhost:3255";
 const MY_APP_API_URL = "https://clientconnectapp.herokuapp.com";
 
 const Checkout = ({ match, history }) => {
   const [bidder, setBidder] = useState("");
-  // const [checkout, setCheckOut] = useState(false);
   const [price, setPrice] = useState("");
   const [projectDetails, setProjectDetails] = useState("");
   const [project, setProject] = useState("");
@@ -85,7 +83,6 @@ const Checkout = ({ match, history }) => {
     console.log(alert);
     setAlert("saved");
     console.log(alert);
-    // setCheckOut(true);
     setShowPaymentDiv(true);
   };
 
@@ -96,7 +93,7 @@ const Checkout = ({ match, history }) => {
       {hidePaypal ? (
         <PaymentMade project={project} bidder={bidder} projectDetails={projectDetails} paymentDetails={paymentDetails} />
       ) : (
-        <Container className=" CheckoutContainer mb-5">
+        <Container className={styles.checkoutContainer}>
           {alert === "confirm" && (
             <Alert className="my-3" variant="success">
               <Alert.Heading>Confirm details of the project with the Freelancer!!</Alert.Heading>
@@ -121,19 +118,23 @@ const Checkout = ({ match, history }) => {
               <Alert.Heading>Confirmation email has been sent to freelancer</Alert.Heading>
             </Alert>
           )}
-          <Row className="d-flex gap-5 py-5 ">
-            <ProjectCheckout project={project} bidder={bidder} price={price} setPrice={setPrice} projectDetails={projectDetails} setProjectDetails={setProjectDetails} makePayment={makePayment} />
-            <ProjectSummary
-              paymentDetails={paymentDetails}
-              showPaymentDiv={showPaymentDiv}
-              setAlert={setAlert}
-              setHidePaypal={setHidePaypal}
-              project={project}
-              bidder={bidder}
-              price={price}
-              setPrice={setPrice}
-              projectDetails={projectDetails}
-            />
+          <Row className="d-flex gap-5 py-5">
+            <div className={styles.checkout}>
+              <div className={styles.projectCheckout}>
+                <ProjectCheckout project={project} bidder={bidder} price={price} setPrice={setPrice} projectDetails={projectDetails} setProjectDetails={setProjectDetails} makePayment={makePayment} />
+              </div>
+                <ProjectSummary
+                  paymentDetails={paymentDetails}
+                  showPaymentDiv={showPaymentDiv}
+                  setAlert={setAlert}
+                  setHidePaypal={setHidePaypal}
+                  project={project}
+                  bidder={bidder}
+                  price={price}
+                  setPrice={setPrice}
+                  projectDetails={projectDetails}
+                />
+            </div>
           </Row>
         </Container>
       )}
