4c1d361ae6912c0a5247f4f6d43baef18e470571
beejay321 <jibodu.busola@gmail.com>
2022-08-01 23:27:43 +0200

    Update MyProjects.jsx

--- src/profile/MyProjects.jsx
+++ src/profile/MyProjects.jsx
@@ -9,7 +9,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const MyProjects = (props) => {
   return (
     <>
-      <div className=" my-2 py-2 px-1 profileColumn " style={{ minHeight: "15rem" }}>
+      <div className=" " style={{ minHeight: "15rem" }}>
         <div className=" mx-2 d-flex justify-content-between ">
           <h4>{props.title}</h4>
           {localStorage.getItem("id") === props.user._id ? (
