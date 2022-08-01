4378daa7d9bd96c3f036a00ab71dbddaed3eb1e2
beejay321 <jibodu.busola@gmail.com>
2022-08-01 23:25:05 +0200

    Update ExperienceCard.jsx

--- src/profile/ExperienceCard.jsx
+++ src/profile/ExperienceCard.jsx
@@ -2,31 +2,31 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import "./profilepage.css";
 import EditExperience from "./EditExperience";
+import styles from "./profile.module.css";
 
 const ExperienceCard = (props) => {
   return (
     <>
-      <div className=" my-2 py-2 px-1 profileColumn " style={{ minHeight: "15rem" }}>
-        <div className=" mx-2 d-flex justify-content-between ">
+      <div className=" p-3 " style={{ minHeight: "15rem" }}>
+        <div className={`mx-2 d-flex justify-content-between ${styles.reviewTitle}`}>
           <h4>{props.title}</h4>
-          <div className=" mx-1 d-flex gap-3 ">
+          <div className="mx-1 d-flex gap-3">
             {localStorage.getItem("id") === props.user._id ? <EditExperience title="Add" user={props.user} /> : ""}
             {localStorage.getItem("id") === props.user._id ? <EditExperience title="Edit" /> : ""}
           </div>
         </div>
-        <hr className=" my-2 " />
         <div>
-          {props.experience ? (
+          {props.experience === [] ? (
             <div>
               {props.experience &&
                 props.experience.map((exp) => (
-                  <div key={exp._id} className=" mx-2 py-2 d-flex justify-content-between ">
+                  <div key={exp._id} className="mx-2 py-2 d-flex justify-content-between">
                     <div>
-                      <Card className="profileCards" style={{ width: "18rem" }}>
-                        <Card.Body className="profileCards">
-                          <Card.Title>{exp.position}</Card.Title>
-                          <Card.Subtitle className="mb-2 text-muted">{exp.company}</Card.Subtitle>
-                          <Card.Text className="  d-flex gap-2 ">
+                      <Card className={styles.profileCards} style={{ width: "18rem" }}>
+                        <Card.Body className={styles.profileCards}>
+                          <Card.Title className={styles.title}>{exp.position}</Card.Title>
+                          <Card.Subtitle className={`mb-2 text-muted${styles.text}`}>{exp.company}</Card.Subtitle>
+                          <Card.Text className={`${styles.text}`}>
                             <span>{exp.city}</span>
                             <span>{exp.country}</span>
                           </Card.Text>
@@ -37,9 +37,34 @@ const ExperienceCard = (props) => {
                 ))}
             </div>
           ) : (
-            <div className="  mt-5 d-flex justify-content-center ">
-              <p>No Experience Yet</p>
-            </div>
+            <>
+              <div className=" mx-2 py-3 d-flex justify-content-between ">
+                <div>
+                  <Card className={styles.profileCards} style={{ width: "18rem" }}>
+                    <Card.Body className={styles.profileCards}>
+                      <Card.Title className={styles.title}>Project manager</Card.Title>
+                      <Card.Subtitle className={`mb-2 text-muted${styles.text}`}>Grayers GmbH</Card.Subtitle>
+                      <Card.Text className={`${styles.text}`}>
+                        <span>Barcelona Spain</span>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </div>
+              </div>
+              <div className="mx-2 py-3 d-flex justify-content-between">
+                <div>
+                  <Card className={styles.profileCards} style={{ width: "18rem" }}>
+                    <Card.Body className={styles.profileCards}>
+                      <Card.Title className={styles.title}>Project designer</Card.Title>
+                      <Card.Subtitle className={`mb-2 text-muted${styles.text}`}>Plink</Card.Subtitle>
+                      <Card.Text className={`${styles.text}`}>
+                        <span>Barcelona Spain</span>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </div>
+              </div>
+            </>
           )}
         </div>
       </div>
