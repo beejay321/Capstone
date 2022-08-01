4ed9aa7d37fe197ce464922929f64f6324aac53d
beejay321 <jibodu.busola@gmail.com>
2022-08-01 23:25:30 +0200

    Update PublicationCard.jsx

--- src/profile/PublicationCard.jsx
+++ src/profile/PublicationCard.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import "./profilepage.css";
 import Edit from "./Edit";
+import styles from "./profile.module.css";
+
 
 const PublicationCard = (props) => {
   return (
     <>
-      <div className=" my-2 py-2 px-1 profileColumn " style={{ minHeight: "15rem" }}>
-        <div className=" mx-2 d-flex justify-content-between ">
+      <div className="p-3 " style={{ minHeight: "15rem" }}>
+        <div className={`mx-2 d-flex justify-content-between ${styles.reviewTitle}`}>
           <h4>{props.title}</h4>
           <div className=" mx-1 d-flex gap-2 ">
             {localStorage.getItem("id") === props.user._id ? <Edit title="Add" /> : ""}
             {localStorage.getItem("id") === props.user._id ? <Edit title="Edit" /> : ""}
           </div>{" "}
         </div>
-        <hr className=" my-2 " />
         <div>
           <div className="  mt-5 d-flex justify-content-center ">
             <p>No Publications Yet</p>
