ed987b01f28f0530646c0e6275e2094806da0ab9
beejay321 <jibodu.busola@gmail.com>
2022-08-03 13:23:40 +0200

    Update EditProject.jsx

--- src/components/ShowDetail/EditProject.jsx
+++ src/components/ShowDetail/EditProject.jsx
@@ -12,11 +12,28 @@ function EditProject(props) {
   const [description, setDescription] = useState(props.project.Description);
   const [summary, setSummary] = useState(props.project.summary);
   const [location, setLocation] = useState(props.project.location);
-  // const [file, setFile] = useState([]);
+  const [file, setFile] = useState([]);
+  const [images, setImage] = useState("https://res.cloudinary.com/dvyids286/image/upload/v1659352336/CapstoneProjects/yquaj1vvqisfuofoxmov.jpg");
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const selectImage = (files) => {
+    // e.preventDefault();
+    const data = files[0];
+    console.log("image", data);
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setImage(reader.result);
+      }
+    };
+    reader.readAsDataURL(files[0]);
+    const formData = new FormData();
+    formData.append("file", files[0]);
+    setFile(formData);
+  };
 
   const editProject = async (e) => {
     try {
@@ -38,6 +55,15 @@ function EditProject(props) {
       });
       console.log(response);
       if (response.ok) {
+        const newResponse = await fetch(`${MY_APP_API_URL}/projects/${props.project._id}/uploadFile`, {
+          method: "PUT",
+          body: file,
+        });
+        if (newResponse.ok) {
+          alert("Sucessfully posted");
+        } else {
+          console.log("File was not uploaded!");
+        }
         console.log("Updated");
         handleClose();
       }
@@ -45,6 +71,7 @@ function EditProject(props) {
       console.log(error);
     }
   };
+
   const deleteProject = async (e) => {
     // e.preventDefault();
     try {
@@ -71,24 +98,27 @@ function EditProject(props) {
         <span>Edit Project</span>
       </div>
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Edit Project</Modal.Title>
+        <Modal.Header>
+          <Modal.Title className={styles.modalTitle}>Edit Project</Modal.Title>
+          <div className={styles.close} onClick={handleClose}>
+            <i class="bi bi-x-lg"></i>
+          </div>
         </Modal.Header>
         <Form onSubmit={props.handleSubmit}>
           <Modal.Body>
             <Form.Group className="mb-3" controlId="formGridAddress1">
-              <Form.Label>Title</Form.Label>
+              <Form.Label className={styles.modalText}>Title</Form.Label>
               <Form.Control required placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} type="text" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formGridAddress1">
-              <Form.Label>Summary</Form.Label>
+              <Form.Label className={styles.modalText}>Summary</Form.Label>
               <Form.Control placeholder="a short description of project" required value={summary} onChange={(e) => setSummary(e.target.value)} />
             </Form.Group>
             <Row className="">
               <Col>
                 <Form.Group as={Col} controlId="formGridEmail">
-                  <Form.Label>Category</Form.Label>
+                  <Form.Label className={styles.modalText}>Category</Form.Label>
                 </Form.Group>
 
                 <div className="form-row">
@@ -118,7 +148,7 @@ function EditProject(props) {
 
             <Row className="mb-3">
               <Form.Group className="mb-3" controlId="formGridAddress1">
-                <Form.Label>Description</Form.Label>
+                <Form.Label className={styles.modalText}>Description</Form.Label>
                 <Form.Control
                   type="text"
                   placeholder="A more detailed description of your project"
@@ -130,20 +160,44 @@ function EditProject(props) {
                 />
               </Form.Group>
             </Row>
+
+            <div>
+              <Button className={styles.fileInput}>
+                <div id="selectimages">
+                  <form enctype="multipart/form-data" method="post" name="fileinfo">
+                    <input
+                      id="post-file"
+                      type="file"
+                      name="file"
+                      onChange={(e) => {
+                        selectImage(e.target.files);
+                      }}
+                      required
+                    />
+                    <label for="post-file">
+                      <div className={styles.fileText}>Upload File</div>
+                    </label>
+                  </form>
+                </div>
+              </Button>
+            </div>
+            <div className={styles.fileDisplay}>
+              <img className={styles.fileDisplayImage} src={images} alt="" />
+            </div>
             {/* {props.formType === 'edit' && ( */}
-            <Form.Group className="mt-3">
+            {/* <Form.Group className="mt-3">
               <Form.Control id="picture" type="file" onChange={props.selectImage} />
-            </Form.Group>
+            </Form.Group> */}
             {/* )} */}
           </Modal.Body>
           <Modal.Footer className="d-flex justify-content-between gap-1">
             <div className="">
               <Button variant="danger" type="submit" onClick={deleteProject}>
-                Delete{" "}
+                <i class="bi bi-trash3-fill"></i>{" "}
               </Button>
             </div>
             <div className="d-flex justify-content-end gap-1">
-              <Button variant="success" type="submit" onClick={editProject}>
+              <Button className={styles.saveBtn} variant="success" type="submit" onClick={editProject}>
                 Edit
               </Button>
             </div>
