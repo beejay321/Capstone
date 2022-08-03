22befcab0b8c627d80efa43ce17bddc43297105c
beejay321 <jibodu.busola@gmail.com>
2022-08-03 12:47:19 +0200

    Update MyProfile.jsx

--- src/profile/MyProfile.jsx
+++ src/profile/MyProfile.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import "./profilepage.css";
+import Skeleton from "react-loading-skeleton";
 import styles from "./profile.module.css";
 import NavBar from "../components/NavBar";
 import MyProfileCard from "./MyProfileCard";
@@ -28,6 +29,8 @@ const MyProfile = (props) => {
   const [projects, setProject] = useState([]);
   const [profile, setProfile] = useState("profile");
   const [myBids, setMyBids] = useState([]);
+  const [isLoading, setIsLoading] = useState("");
+
   // const [skills, setSkills] = useState([]);
   // const [showChatList, setShowChatList] = useState(false);
   // const [language, setLanguage] = useState([]);
@@ -38,6 +41,8 @@ const MyProfile = (props) => {
   let match = useRouteMatch();
 
   useEffect(() => {
+    setIsLoading(true);
+
     const getProfile = async () => {
       try {
         let response = await fetch(`${MY_APP_API_URL}/users/${match.params.id}`, {
@@ -55,6 +60,7 @@ const MyProfile = (props) => {
           setMyBids(data.myBids);
           setEducation(data.education);
           setExperience(data.experience);
+          setIsLoading(false);
         }
       } catch (error) {
         console.log(error);
@@ -72,83 +78,132 @@ const MyProfile = (props) => {
       <div className={styles.profilePage}>
         <Container className="" style={{ minHeight: "100vh" }}>
           <Row className={styles.profileRow}>
-            <div className={styles.profileDashboard}>
-              <div className={styles.profileOwner}>
-                <div className={styles.profileOwnerImage}>
-                  <div className={styles.profileImageDiv}>
-                    <Image className={styles.profileImage} src={user.avatar} fluid />
+            {isLoading ? (
+              <>
+                {" "}
+                <div className={styles.profileDashboard}>
+                  <div className={styles.profileOwner}>
+                    <div className={styles.profileOwnerImage}>
+                      <div className={styles.profileImageDiv}>
+                        <Skeleton circle height="100%" containerClassName="avatar-skeleton" />
+                      </div>
+                    </div>
+                    <div className={`d-flex justify-content-center ${styles.title}`}>
+                      <h5>
+                        <Skeleton width={120} height={18} />
+                      </h5>
+                    </div>
+                    <div className={`d-flex justify-content-center ${styles.text}`}>
+                      <Skeleton width={150} height={18} />
+                    </div>
+                    <div className={`d-flex justify-content-center ${styles.name}`}>
+                      {" "}
+                      <Skeleton width={100} height={18} />
+                    </div>
+                  </div>
+                  <div className={styles.tabsDiv}>
+                    <div className={styles.tabs} onClick={() => setProfile("profile")}>
+                      <Skeleton width={180} height={18} />
+                    </div>
+                    <div className={styles.tabs} onClick={() => setProfile("projects")}>
+                      <Skeleton width={180} height={18} />
+                    </div>
+                    <div className={styles.tabs} onClick={() => setProfile("bids")}>
+                      <Skeleton width={180} height={18} />
+                    </div>
+                    <div className="m-3">
+                      <Skeleton width={320} height={400} />
+                    </div>
                   </div>
                 </div>
-                <div className={`d-flex justify-content-center ${styles.title}`}>
-                  <h5>
-                    {user.firstname} {user.lastname}
-                  </h5>
-                </div>
-                <div className={`d-flex justify-content-center ${styles.text}`}>
-                  <h6>{user.occupation}</h6>
-                </div>
-                <div className={`d-flex justify-content-center ${styles.name}`}>{user.location}</div>
-                <div className={styles.editBtn}>
-                  <i className="bi bi-pencil-fill" onClick={() => props.history.push(`/updateProfile/${user._id}`)}></i>
-                </div>
-              </div>
+                <div className={styles.profileColumn}></div>
+              </>
+            ) : (
+              <>
+                <div className={styles.profileDashboard}>
+                  <div className={styles.profileOwner}>
+                    <div className={styles.profileOwnerImage}>
+                      <div className={styles.profileImageDiv}>
+                        <Image className={styles.profileImage} src={user.avatar} fluid />
+                      </div>
+                    </div>
+                    <div className={`d-flex justify-content-center ${styles.title}`}>
+                      <h5>
+                        {user.firstname} {user.lastname}
+                      </h5>
+                    </div>
+                    <div className={`d-flex justify-content-center ${styles.text}`}>
+                      <h6>{user.occupation}</h6>
+                    </div>
+                    <div className={`d-flex justify-content-center ${styles.name}`}>{user.location}</div>
+                    <div className={styles.editBtn}>
+                      <i className="bi bi-pencil-fill" onClick={() => props.history.push(`/updateProfile/${user._id}`)}></i>
+                    </div>
+                  </div>
+                  {/*                Thetabs                 */}
 
-              {/*                Thetabs                 */}
-              <div className={styles.tabsDiv}>
-                <div className={styles.tabs} onClick={() => setProfile("profile")}>
-                  <p className="">Professional Summary</p>
-                </div>
-                <div className={styles.tabs} onClick={() => setProfile("projects")}>
-                  <p className="">Projects</p>
-                </div>
-                <div className={styles.tabs} onClick={() => setProfile("bids")}>
-                  <p className="">Offers</p>
-                </div>
-              </div>
-              {/*            Reviews               */}
-              <div>
-                <Reviews user={user} />
-              </div>
+                  <div className={styles.tabsDiv}>
+                    <div className={styles.tabs} onClick={() => setProfile("profile")}>
+                      <p className="">Professional Summary</p>
+                    </div>
+                    <div className={styles.tabs} onClick={() => setProfile("projects")}>
+                      <p className="">Projects</p>
+                    </div>
+                    <div className={styles.tabs} onClick={() => setProfile("bids")}>
+                      <p className="">Offers</p>
+                    </div>
+                  </div>
+                  {/*            Reviews               */}
+                  <div>
+                    <Reviews user={user} />
+                  </div>
 
-              <div className={styles.mapouter}>
-                <div className={styles.gmapCanvas}>
-                  <iframe
-                    width="370"
-                    height="500"
-                    id="gmap_canvas"
-                    src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed"
-                    frameborder="0"
-                    scrolling="no"
-                    marginheight="0"
-                    marginwidth="0"
-                  ></iframe>
-                  <br />
+                  <div className={styles.mapouter}>
+                    <div className={styles.gmapCanvas}>
+                      <iframe
+                        width="370"
+                        height="500"
+                        id="gmap_canvas"
+                        src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                        frameborder="0"
+                        scrolling="no"
+                        marginheight="0"
+                        marginwidth="0"
+                      ></iframe>
+                      <br />
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
 
-            {/********************** Profile details *************************/}
-            <div className={styles.profileColumn}>
-              {profile === "projects" && <MyProfileCard title="Projects" user={user} content={<MyProjects title="Projects" projects={projects} user={user} />} />}
-              {profile === "bids" && <MyProfileCard title="Projects" user={user} content={<MyBids title="Bids" myBids={myBids} user={user} />} />}
-              {profile === "profile" && (
-                <>
-                  <MyProfileCard title="Experience" user={user} content={<ExperienceCard title="Experience" user={user} experience={experience} />} />
-                  <hr className="" />
-                  <MyProfileCard title="Education" user={user} content={<EducationCard title="Education" user={user} education={education} />} />
-                  <hr className="" />
-                  <MyProfileCard title="Skills" user={user} content={<SkillsCard title="Skills" user={user} />} />
-                  <hr className="" />
-                  <MyProfileCard title="Publication" user={user} content={<PublicationCard title="Publication" user={user} />} />
-                  <hr className="" />
-                  <MyProfileCard title="Certification" content={<CertificationCard title="Certifications" user={user} />} />
-                </>
-              )}
-              {/* {profile === "messages" && <Messages />} */}
-            </div>
-            <Message user={user} history={props.history} />
+                {/* </>
+           
+
+            <> */}
+                {/* Loaders */}
+                {/********************** Profile details *************************/}
+                <div className={styles.profileColumn}>
+                  {profile === "projects" && <MyProfileCard title="Projects" user={user} content={<MyProjects title="Projects" projects={projects} user={user} />} />}
+                  {profile === "bids" && <MyProfileCard title="Projects" user={user} content={<MyBids title="Bids" myBids={myBids} user={user} />} />}
+                  {profile === "profile" && (
+                    <>
+                      <MyProfileCard title="Experience" user={user} content={<ExperienceCard title="Experience" user={user} experience={experience} />} />
+                      <hr className="" />
+                      <MyProfileCard title="Education" user={user} content={<EducationCard title="Education" user={user} education={education} />} />
+                      <hr className="" />
+                      <MyProfileCard title="Skills" user={user} content={<SkillsCard title="Skills" user={user} />} />
+                      <hr className="" />
+                      <MyProfileCard title="Publication" user={user} content={<PublicationCard title="Publication" user={user} />} />
+                      <hr className="" />
+                      <MyProfileCard title="Certification" content={<CertificationCard title="Certifications" user={user} />} />
+                    </>
+                  )}
+                  {/* {profile === "messages" && <Messages />} */}
+                </div>
+              </>
+            )}
           </Row>
 
+          <Message user={user} history={props.history} />
           <Row></Row>
         </Container>
       </div>
