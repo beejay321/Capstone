a2af9a877ff94ec356c6e9d8da7ba87ebac4134c
Oluwabusola Jibodu <jibodu.busola@gmail.com>
2023-08-02 23:33:12 +0200

    Update ChatRooms.jsx

--- src/components/Message/ChatRooms.jsx
+++ src/components/Message/ChatRooms.jsx
@@ -5,7 +5,8 @@ import "react-chat-elements/dist/main.css";
 import styles from "./message.module.css";
 
 const userId = localStorage.getItem("id");
-const MY_APP_API_URL = "https://clientconnectapp.herokuapp.com";
+//const MY_APP_API_URL = "https://clientconnectapp.herokuapp.com";
+const MY_APP_API_URL = "https://clientconnect-b57f56bb1351.herokuapp.com";
 
 function ChatRooms({ showChatBox, showNotification }) {
   const [chats, setChats] = useState([]);
